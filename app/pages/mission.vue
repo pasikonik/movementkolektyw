e5e5c38135043fd75108517b3d6197d64a65ae58
Michał Olejniczak <blind.x6@gmail.com>
2025-03-17 21:42:00 +0100

    Add Mission page

--- app/pages/mission.vue
+++ app/pages/mission.vue
@@ -1,7 +1,42 @@
 <template>
     <UContainer class="p-7 text-center">
-        <h2 class="text-4xl sm:text-6xl font-bold">Naszą misją jest ćwiczyć ćwiczyć i ćwiczyć.</h2>
-    
-        <p>i trochę zabawy...</p>
+        <section class="max-w-3xl mx-auto py-12 px-4">
+            <h1 class="text-4xl font-bold text-center mb-8">Nasza Misja</h1>
+
+            <div class="flex flex-col md:flex-row items-center mb-12">
+                <img src="/teamwork.png" alt="Równość i współpraca" class="w-24 h-24 mx-auto md:mx-0 md:mr-6">
+                <p class="text-lg text-gray-800"> Jesteśmy grupą pasjonatów ruchu, która praktykuje
+                    <strong>movement</strong>. Naszą misją jest stworzenie
+                    społeczności, która jest samoorganizującą się jednostką, bez wyznaczonego lidera. Każdy jest równy,
+                    każdy ma możliwość prowadzenia zajęć i dzielenia się swoją wiedzą oraz doświadczeniem.
+                </p>
+            </div>
+
+            <div class="flex flex-col md:flex-row-reverse items-center mb-12">
+                <img src="/movement.png" alt="Dostępność" class="w-24 h-24 mx-auto md:mx-0 md:ml-6">
+                <p class="text-lg text-gray-800">
+                    Wierzymy, że rozwój ruchowy to fascynująca, niekończąca się podróż. Nasze ciała są niezwykle złożone
+                    i
+                    odkrywanie ich możliwości traktujemy jak wspaniałą przygodę, pełną nowych odkryć i wyzwań. Dlatego
+                    praktykujemy wszechstronny, różnorodny ruch, rozwijając się na wielu płaszczyznach.</p>
+            </div>
+
+            <div class="flex flex-col md:flex-row items-center">
+                <img src="/solidarity.png" alt="Wszechstronny rozwój ruchowy" class="w-24 h-24 mx-auto md:mx-0 md:mr-6">
+                <p class="text-lg"> Równość i dostępność są fundamentem naszej grupy. Nie czerpiemy korzyści finansowych
+                    – zbieramy jedynie
+                    fundusze na pokrycie kosztów wynajmu salki. Chcemy, aby każdy, bez względu na swój budżet, mógł do
+                    nas
+                    dołączyć i wspólnie eksplorować ruch.</p>
+            </div>
+
+            <p class="text-xl font-semibold text-center mt-10 px-18">
+                Dołącz do nas –
+                współtwórzmy razem społeczność, w której
+                każdy jest mile widziany! 💙
+            </p>
+
+            <img class="pt-8" src="/ciało.jpeg">
+        </section>
     </UContainer>
 </template>
