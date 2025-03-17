66d9c4f755ffe5a68841c902e1cba86da133b667
Michał Olejniczak <blind.x6@gmail.com>
2025-03-17 21:47:25 +0100

    Update practice page

--- app/pages/practice.vue
+++ app/pages/practice.vue
@@ -1,7 +1,5 @@
 <template>
-    <UContainer class="flex flex-row justify-center">
-        <h3 class="text-3xl font-bold">Plan</h3>
-
+    <div class="flex justify-center items-center p-10">
         <img src="/plan.png" alt="timetable">
-    </UContainer>
+    </div>
 </template>
