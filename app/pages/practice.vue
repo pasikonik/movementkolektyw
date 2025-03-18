63c22a9a82591e71ae70fa30265102805fbcf940
Michał Olejniczak <blind.x6@gmail.com>
2025-03-18 14:22:06 +0100

    Update practice

--- app/pages/practice.vue
+++ app/pages/practice.vue
@@ -1,99 +1,72 @@
 <template>
     <div class="flex flex-col justify-center items-center p-8">
-        <table class=" bg-white border border-gray-300">
+        <div class="text-lg">Plan na: 3-4.2025</div>
+
+        <table class=" bg-white">
             <thead>
-                <tr class="bg-gray-200">
+                <tr>
                     <th class="py-2 px-4 border-b"></th>
-                    <th class="py-2 px-4 border-b">Nr</th>
+                    <th class="py-2 px-4 border-b">Godzina</th>
                     <th class="py-2 px-4 border-b">Blok</th>
-                    <th class="py-2 px-4 border-b">Prowadzący</th>
-                    <th class="py-2 px-4 border-b">Długość</th>
-                    <th class="py-2 px-4 border-b">Przedział</th>
                 </tr>
             </thead>
             <tbody>
                 <tr class="bg-orange-200">
-                    <td class="py-2 px-4 border-b" rowspan="5">
-                        <div class="inline-block transform origin-top-left -rotate-90">Wtorek</div>
+                    <td class="py-2 px-4 border-b border-l" rowspan="5">
+                        <div class="text-xl inline-block transform origin-top-left -rotate-90">Wtorek</div>
                     </td>
-                    <td class="py-2 px-4 border-b">1.</td>
-                    <td class="py-2 px-4 border-b">Rozgrzewka</td>
-                    <td class="py-2 px-4 border-b">Joasia</td>
-                    <td class="py-2 px-4 border-b">15 min</td>
-                    <td class="py-2 px-4 border-b">18:45-19:00</td>
+                    <td class="py-2 px-4 border-b" rowspan="5">
+                        <div>Movement</div>
+                        <div>18:45-20:15</div>
+                    </td>
+                    <td class="py-2 px-4 border-b border-r">Rozgrzewka</td>
                 </tr>
                 <tr class="bg-orange-200">
-                    <td class="py-2 px-4 border-b">2.</td>
-                    <td class="py-2 px-4 border-b">Kopnięcia</td>
-                    <td class="py-2 px-4 border-b">Gzesiu</td>
-                    <td class="py-2 px-4 border-b">20 min</td>
-                    <td class="py-2 px-4 border-b">19:00-19:20</td>
+                    <td class="py-2 px-4 border-b border-r">Nogi - dynamika</td>
                 </tr>
                 <tr class="bg-orange-200">
-                    <td class="py-2 px-4 border-b">3.</td>
-                    <td class="py-2 px-4 border-b">Flow</td>
-                    <td class="py-2 px-4 border-b">Olej</td>
-                    <td class="py-2 px-4 border-b">25 min</td>
-                    <td class="py-2 px-4 border-b">19:20-19:45</td>
+                    <td class="py-2 px-4 border-b border-r">Flow</td>
                 </tr>
                 <tr class="bg-orange-200">
-                    <td class="py-2 px-4 border-b">4.</td>
-                    <td class="py-2 px-4 border-b">Ringi</td>
-                    <td class="py-2 px-4 border-b">Szymon</td>
-                    <td class="py-2 px-4 border-b">30 min</td>
-                    <td class="py-2 px-4 border-b">19:45-20:15</td>
+                    <td class="py-2 px-4 border-b border-r">Ringi</td>
                 </tr>
                 <tr class="bg-orange-200">
-                    <td class="py-2 px-4 border-b">5.</td>
-                    <td class="py-2 px-4 border-b">Relaks</td>
-                    <td class="py-2 px-4 border-b"></td>
-                    <td class="py-2 px-4 border-b">5 min</td>
-                    <td class="py-2 px-4 border-b">20:15-20:20</td>
+                    <td class="py-2 px-4 border-b border-r">Relaks</td>
+                </tr>
+                <tr class="bg-green-200">
+                    <td class="py-2 px-4 border-b border-l" rowspan="8">
+                        <div class="text-xl inline-block transform origin-top-left -rotate-90">Czwartek</div>
+                    </td>
+                    <td class="py-2 px-4 border-b" rowspan="2">
+                        <div>Handstand</div>
+                        <div>17:30-18:30</div>
+                    </td>
+                    <td class="py-2 px-4 border-b border-r">Rozgrzewka</td>
+                </tr>
+                <tr class="bg-green-200">
+                    <td class="py-2 px-4 border-b border-r">Praktyka</td>
                 </tr>
                 <tr class="bg-green-200">
                     <td class="py-2 px-4 border-b" rowspan="6">
-                        <div class="inline-block transform origin-top-left -rotate-90">Czwartek</div>
+                        <div>Movement</div>
+                        <div>18:45-20:15</div>
                     </td>
-                    <td class="py-2 px-4 border-b">1.</td>
-                    <td class="py-2 px-4 border-b">Rozgrzewka</td>
-                    <td class="py-2 px-4 border-b">Paweł</td>
-                    <td class="py-2 px-4 border-b">20 min</td>
-                    <td class="py-2 px-4 border-b">18:45-19:05</td>
+                    <td class="py-2 px-4 border-b border-r">Rozgrzewka</td>
                 </tr>
                 <tr class="bg-green-200">
-                    <td class="py-2 px-4 border-b">2.</td>
-                    <td class="py-2 px-4 border-b">Lokomocje</td>
-                    <td class="py-2 px-4 border-b">Olej</td>
-                    <td class="py-2 px-4 border-b">20 min</td>
-                    <td class="py-2 px-4 border-b">19:05-19:25</td>
+                    <td class="py-2 px-4 border-b border-r">Lokomocje</td>
                 </tr>
                 <tr class="bg-green-200">
-                    <td class="py-2 px-4 border-b">3.</td>
-                    <td class="py-2 px-4 border-b">Impro</td>
-                    <td class="py-2 px-4 border-b"></td>
-                    <td class="py-2 px-4 border-b">10 min</td>
-                    <td class="py-2 px-4 border-b">19:25-19:35</td>
+                    <td class="py-2 px-4 border-b border-r">Improwizacja</td>
                 </tr>
                 <tr class="bg-green-200">
-                    <td class="py-2 px-4 border-b">4.</td>
-                    <td class="py-2 px-4 border-b">Pompki</td>
-                    <td class="py-2 px-4 border-b">Edyta</td>
-                    <td class="py-2 px-4 border-b">15 min</td>
-                    <td class="py-2 px-4 border-b">19:35-19:50</td>
+                    <td class="py-2 px-4 border-b border-r">Ręce - siła</td>
                 </tr>
                 <tr class="bg-green-200">
-                    <td class="py-2 px-4 border-b">5.</td>
-                    <td class="py-2 px-4 border-b">Mobility</td>
-                    <td class="py-2 px-4 border-b">Karol</td>
-                    <td class="py-2 px-4 border-b">20 min</td>
-                    <td class="py-2 px-4 border-b">19:50-20:10</td>
+                    <td class="py-2 px-4 border-b border-r">Mobility</td>
                 </tr>
                 <tr class="bg-green-200">
-                    <td class="py-2 px-4 border-b">6.</td>
-                    <td class="py-2 px-4 border-b">Relaks</td>
-                    <td class="py-2 px-4 border-b"></td>
-                    <td class="py-2 px-4 border-b">5 min</td>
-                    <td class="py-2 px-4 border-b">20:10-20:15</td>
+                    <td class="py-2 px-4 border-b border-r">Relaks</td>
                 </tr>
             </tbody>
         </table>
@@ -106,9 +79,9 @@
                     <path d="M21 10c0 7-9 13-9 13S3 17 3 10a9 9 0 0118 0z" />
                     <circle cx="12" cy="10" r="3" />
                 </svg>
-               <a href="https://maps.app.goo.gl/rdUvGnu2DjSosHtv7" target="_blank"
+                <a href="https://maps.app.goo.gl/rdUvGnu2DjSosHtv7" target="_blank"
                     class="text-gray-700 font-medium hover:text-green-600 transition duration-200">
-                    ul. Wielka 19/8, 61-775 Poznań, Polska
+                    ul. Wielka 19/8 <br> 61-775 Poznań, Polska
                 </a>
             </div>
         </div>
