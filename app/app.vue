934cd5fd0feea03f17e1d7c9e6453caa0e6f0ee2
Michał Olejniczak <blind.x6@gmail.com>
2025-03-20 15:21:16 +0100

    Test light mode

--- app/app.vue
+++ app/app.vue
@@ -5,3 +5,19 @@
     </NuxtLayout>
   </UApp>
 </template>
+
+<script setup>
+useHead({
+  bodyAttrs: { class: "light" },
+});
+</script>
+
+<style>
+html {
+  color-scheme: light;
+}
+
+html.dark {
+  display: none;
+}
+</style>
