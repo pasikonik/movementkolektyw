b81f75a8144acc1dc467289f289945600d9bfb00
Michał Olejniczak <blind.x6@gmail.com>
2025-03-21 17:56:41 +0100

    Improve practice page

--- app/pages/practice.vue
+++ app/pages/practice.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="flex flex-col justify-center items-center p-8">
+    <div class="flex flex-col justify-center items-center py-8 px-0">
 
         <h1 class="text-4xl font-bold text-center mb-8">Zajęcia</h1>
 
-        <table class=" bg-white">
+        <table class="bg-white table-fixed w-full md:w-1/3">
             <thead>
                 <tr>
                     <th class="border-b"></th>
@@ -12,15 +12,17 @@
                 </tr>
             </thead>
             <tbody>
-                <tr class="bg-orange-100">
-                    <td class="pt-12 pl-3 border-b border-l" rowspan="5">
-                        <div class="text-2xl inline-block transform origin-top-left -rotate-45">Wtorek</div>
+                <tr>
+                    <td class="pt-30 pl-3 bg-orange-500 border-b border-l" rowspan="5">
+                        <div
+                            class="text-2xl inline-block transform origin-top-left -rotate-90 w-full h-full flex items-center justify-center uppercase">
+                            Wtorek</div>
                     </td>
-                    <td class="py-2 px-4 text-lg border text-center" rowspan="5">
-                        <div class="uppercase font-semibold">Movement</div>
+                    <td class="py-2 px-4 text-lg bg-orange-300 border text-center" rowspan="5">
+                        <div class="uppercase font-bold">Movement</div>
                         <div>18:45-20:15</div>
                     </td>
-                    <td class="py-2 px-4 border-b border-r">Rozgrzewka</td>
+                    <td class="py-2 px-4 bg-orange-100 border-b border-r">Rozgrzewka</td>
                 </tr>
                 <tr class="bg-orange-100">
                     <td class="py-2 px-4 border-b border-r">Nogi - dynamika</td>
@@ -34,25 +36,27 @@
                 <tr class="bg-orange-100">
                     <td class="py-2 px-4 border-b border-r">Relaks</td>
                 </tr>
-                <tr class="bg-green-100">
-                    <td class="pt-6 pl-3 border-b border-l" rowspan="8">
-                        <div class="text-2xl inline-block transform origin-top-left -rotate-45">Czwartek</div>
+                <tr>
+                    <td class="pt-6 pl-3 bg-green-400 border-b border-l" rowspan="8">
+                        <div
+                            class="text-2xl inline-block transform origin-top-left -rotate-90 w-full h-full flex items-center justify-center uppercase">
+                            Czwartek</div>
                     </td>
-                    <td class="py-2 px-4 text-lg border text-center" rowspan="2">
-                        <div class="uppercase font-semibold">Handstand</div>
+                    <td class="py-2 px-4 text-lg bg-green-300 border text-center" rowspan="2">
+                        <div class="uppercase font-bold">Handstand</div>
                         <div>17:30-18:30</div>
                     </td>
-                    <td class="py-2 px-4 border-b border-r">Rozgrzewka</td>
+                    <td class="py-2 px-4 bg-green-100 border-b border-r">Rozgrzewka</td>
                 </tr>
                 <tr class="bg-green-100">
                     <td class="py-2 px-4 border-b border-r">Praktyka</td>
                 </tr>
-                <tr class="bg-green-100">
+                <tr class="bg-green-300">
                     <td class="py-2 px-4 text-lg border text-center" rowspan="6">
-                        <div class="uppercase font-semibold">Movement</div>
+                        <div class="uppercase font-bold">Movement</div>
                         <div>18:45-20:15</div>
                     </td>
-                    <td class="py-2 px-4 border-b border-r">Rozgrzewka</td>
+                    <td class="py-2 px-4 bg-green-100 border-b border-r">Rozgrzewka</td>
                 </tr>
                 <tr class="bg-green-100">
                     <td class="py-2 px-4 border-b border-r">Sekwencje</td>
@@ -72,9 +76,13 @@
             </tbody>
         </table>
 
-        <div class="mt-2 text-lg">Plan na: 3-4.2025</div>
 
-        <div class="location p-10">
+        <div class="mt-2 font-semibold text-lg">Plan na: 3-4.2025</div>
+
+        <div class="p-10">
+            <h3 class="text-2xl text-center font-bold mb-2">
+                A gdzie? A tu:
+            </h3>
             <div class="flex items-center space-x-4">
                 <svg xmlns="http://www.w3.org/1000/svg" class="h-6 w-6 text-green-500" viewBox="0 0 24 24" fill="none"
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
@@ -89,3 +97,20 @@
         </div>
     </div>
 </template>
+
+<style scoped>
+td:first-child,
+th:first-child {
+    width: 45px;
+}
+
+td:nth-child(even),
+th:nth-child(even) {
+    width: 140px;
+}
+
+td:last-child,
+th:last-child {
+    width: 120px;
+}
+</style>
