4ef1e0f3dc78fbd909341367b516fb1e30608552
Michał Olejniczak <blind.x6@gmail.com>
2025-08-28 16:51:18 +0200

    better formatting

--- app/pages/practice.vue
+++ app/pages/practice.vue
@@ -13,16 +13,11 @@
       <tbody>
         <tr>
           <td class="pt-30 pl-3 bg-orange-400 border-b border-l" rowspan="5">
-            <div
-              class="text-2xl inline-block transform origin-[30%_5%] -rotate-90 w-full h-full uppercase"
-            >
+            <div class="text-2xl inline-block transform origin-[30%_5%] -rotate-90 w-full h-full uppercase">
               Wtorek
             </div>
           </td>
-          <td
-            class="py-2 px-4 text-lg bg-orange-300 border text-center"
-            rowspan="6"
-          >
+          <td class="py-2 px-4 text-lg bg-orange-300 border text-center" rowspan="6">
             <div class="uppercase font-bold">Movement</div>
             <div>18:45-20:15</div>
           </td>
@@ -32,7 +27,7 @@
         </tr>
         <tr class="bg-orange-100">
           <td class="py-2 px-4 border-b border-r text-sm">Partering</td>
-        </tr>        
+        </tr>
         <tr class="bg-orange-100">
           <td class="py-2 px-4 border-b border-r text-sm">Floorwork</td>
         </tr>
@@ -44,9 +39,7 @@
         </tr>
         <tr>
           <td class="pt-6 pl-3 bg-green-400 border-b border-l" rowspan="6">
-            <div
-              class="text-2xl inline-block transform origin-[75%_65%] -rotate-90 w-full h-full uppercase"
-            >
+            <div class="text-2xl inline-block transform origin-[75%_65%] -rotate-90 w-full h-full uppercase">
               Czwartek
             </div>
           </td>
@@ -83,24 +76,13 @@
     <div class="p-10">
       <h4 class="text-xl font-semibold mb-2">Studio Movement Poznań</h4>
       <div class="flex items-center space-x-4">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6 text-green-500"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-green-500" viewBox="0 0 24 24" fill="none"
+          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
           <path d="M21 10c0 7-9 13-9 13S3 17 3 10a9 9 0 0118 0z" />
           <circle cx="12" cy="10" r="3" />
         </svg>
-        <a
-          href="https://maps.app.goo.gl/rdUvGnu2DjSosHtv7"
-          target="_blank"
-          class="text-gray-700 font-medium hover:text-green-600 transition duration-100"
-        >
+        <a href="https://maps.app.goo.gl/rdUvGnu2DjSosHtv7" target="_blank"
+          class="text-gray-700 font-medium hover:text-green-600 transition duration-100">
           ul. Wielka 19/8 <br />
           61-775 Poznań, Polska
         </a>
