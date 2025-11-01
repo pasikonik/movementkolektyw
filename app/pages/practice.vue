3490cc42067c4d5cd53233a4fc6727fcd5db576d
Michał Olejniczak <blind.x6@gmail.com>
2025-11-01 12:40:14 +0100

    update plan span

--- app/pages/practice.vue
+++ app/pages/practice.vue
@@ -141,7 +141,7 @@
                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
                 </svg>
-                <span>Plan na: wrzesień-październik 2025</span>
+                <span>Plan na: listopad-grudzień 2025</span>
             </div>
         </div>
     </div>
