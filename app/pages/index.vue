21e44df428181c9af95d612e1d186fe157e29610
Michał Olejniczak <blind.x6@gmail.com>
2026-03-06 14:04:30 +0100

    fetchpriority define

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,16 +10,26 @@ import HeroSection from '~/components/HeroSection.vue';
 const config = useRuntimeConfig();
 const siteUrl = config.public.siteUrl;
 
-useSeoMeta({
-  title: 'Movement Kolektyw - Społeczność, Treningi i Warsztaty | Poznań',
-  description: 'Szukasz zajęć Movement w Poznaniu? Dołącz do Movement Kolektyw. Rozwijaj ciało poprzez naturalny ruch, mobility, akrobatykę i siłę. Sprawdź grafik zajęć i trenuj z nami!',
+useHead({
+   link: [
+      {
+         rel: 'preload',
+         as: 'image',
+         href: '/_netlify/images?w=1536&url=%2Ftlo.webp',
+         fetchpriority: 'high'
+      }
+   ]
+})
 
-  ogTitle: 'Movement Kolektyw - Dołącz do społeczności ruchu',
-  ogDescription: 'Odkryj możliwości swojego ciała. Treningi grupowe, warsztaty i pasjonaci ruchu w sercu Poznania.',
-  ogImage: `${siteUrl}/name.webp`,
-  ogUrl: siteUrl,
-  ogType: 'website',
-  ogLocale: 'pl_PL',
+useSeoMeta({
+   title: 'Movement Kolektyw - Społeczność, Treningi i Warsztaty | Poznań',
+   description: 'Szukasz zajęć Movement w Poznaniu? Dołącz do Movement Kolektyw. Rozwijaj ciało poprzez naturalny ruch, mobility, akrobatykę i siłę. Sprawdź grafik zajęć i trenuj z nami!',
+
+   ogTitle: 'Movement Kolektyw - Dołącz do społeczności ruchu',
+   ogDescription: 'Odkryj możliwości swojego ciała. Treningi grupowe, warsztaty i pasjonaci ruchu w sercu Poznania.',
+   ogImage: `${siteUrl}/name.webp`,
+   ogUrl: siteUrl,
+   ogType: 'website',
+   ogLocale: 'pl_PL',
 })
 </script>
-
